a66b1ec2c19406cf9450b09b2e0273b23d67ecbb
Mannat Singh <mannatwadhwa14@gmail.com>
2026-01-15 18:07:12 +0000

    Minor changes

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,7 @@ module Main (main) where
 
 import Lib (mainSimulation)
 
+-- | The entry point for the executable.
+-- Delegates directly to the library's simulation logic.
 main :: IO ()
 main = mainSimulation
